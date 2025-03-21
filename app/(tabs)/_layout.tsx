eef18575b4e13a3e2e876c00f897fab47e3bc124
tobicodinghub <hornbogent@helmholtzschule.de>
2025-03-21 21:06:59 +0100

    setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="hello"
+        options={{
+          title: 'Hello',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="hand.wave.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
